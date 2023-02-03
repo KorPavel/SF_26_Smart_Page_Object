53b040f19a7529d4fc6e10518db169bc4c6fc3f1
KorPavel <kor-p@bk.ru>
2023-02-03 12:53:43 +0300

    Create auth_page.py
    Определение веб-элементов для страницы авторизации сайта "Дом питомца"

---
+++ pages/auth_page.py
@@ -0,0 +1,13 @@
+from pages.base import WebPage
+from pages.elements import WebElement
+
+
+class AuthPage(WebPage):
+
+    def __init__(self, web_driver, url=''):
+        url = 'https://petfriends.skillfactory.ru/login'
+        super().__init__(web_driver, url)
+
+    email = WebElement(id='email')
+    password = WebElement(id='pass')
+    btn = WebElement(class_name='btn.btn-success')
